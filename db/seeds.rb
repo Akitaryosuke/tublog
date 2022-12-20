c61a31228b250b78ec96d3725fcc92128639c899
EC2 Default User <ec2-user@ip-172-31-32-215.ap-northeast-1.compute.internal>
2022-12-20 11:03:11 +0000

    [Fix] Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
 Customer.create!(
-  name: '管理者',
-  introduction: 'ss',
-  email: 'admin@admin',
-  password: 'testtest',
-  admin: true
+  {name: '管理者',  introduction: 'ss',  email: 'admin@admin',  password: 'testtest',avater_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg"), admin: true}
 )
